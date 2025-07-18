03bbe3dddeeb7408a8eb81bc9d0ab5c702c36812
Ahsan-26 <ahsankshmiri@gmail.com>
2025-07-18 18:01:19 +0500

    build solved

--- app/quote-confirmation/page.js
+++ app/quote-confirmation/page.js
@@ -1,7 +1,12 @@
 'use client';
-
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
 import QuoteConfirmation from '@/components/QuoteConfirmation';
 
 export default function QuoteConfirmationPage() {
-  return <QuoteConfirmation />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <QuoteConfirmation />
+    </Suspense>
+  );
 }
